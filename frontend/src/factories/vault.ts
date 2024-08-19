2479b0da622fbdd3526e6b732c652567a1891629
Rizky Novando Priyadi <rizkynovando@gmail.com>
2024-08-19 22:13:18 +0700

    feature: vault endpoints

---
+++ frontend/src/factories/vault.ts
@@ -0,0 +1,42 @@
+import libFetch from '@arutek/core-app/libraries/fetch'
+
+type CreatePayloadType = {
+  name: string
+  credential: CredentialType
+}
+type CredentialType = {
+  name: string
+  password: string
+  credential: string
+  url: string
+  note: string
+}
+
+const apiUrl = `${import.meta.env.VITE_API_URL}/vault`
+
+export default {
+  getAll ():Promise<ResponseType> {
+    return libFetch.getDataLogged(apiUrl)
+  },
+  getOne (id: string):Promise<ResponseType> {
+    return libFetch.getDataLogged(`${apiUrl}/${id}`)
+  },
+  create (payload: CreatePayloadType):Promise<ResponseType> {
+    return libFetch.postDataLogged(`${apiUrl}`, payload)
+  },
+  createCredential (vaultId: string, payload: CreatePayloadType):Promise<ResponseType> {
+    return libFetch.postDataLogged(`${apiUrl}/${vaultId}`, payload)
+  },
+  update (vaultId: string, payload: { name: string }):Promise<ResponseType> {
+    return libFetch.putDataLogged(`${apiUrl}/${vaultId}`, payload)
+  },
+  updateCredential (vaultId: string, credentialId: string, payload: CreatePayloadType):Promise<ResponseType> {
+    return libFetch.putDataLogged(`${apiUrl}/${vaultId}/${credentialId}`, payload)
+  },
+  delete (vaultId: string):Promise<ResponseType> {
+    return libFetch.delDataLogged(`${apiUrl}/${vaultId}`)
+  },
+  deleteCredential (vaultId: string, credentialId: string):Promise<ResponseType> {
+    return libFetch.delDataLogged(`${apiUrl}/${vaultId}/${credentialId}`)
+  },
+}
