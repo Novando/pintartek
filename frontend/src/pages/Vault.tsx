ec9dd8853d175157ef26f84d0d217560a906b855
novando <rizkynovando@gmail.com>
2024-08-20 18:09:06 +0700

    feature: vault from server

--- frontend/src/pages/Vault.tsx
+++ frontend/src/pages/Vault.tsx
@@ -2,12 +2,14 @@ import { useState, useEffect, useRef, ChangeEvent } from 'react'
 import libDate from '@arutek/core-app/libraries/date'
 import callModal from '@src/utils/call-modal'
 import {Link, useNavigate, useParams} from 'react-router-dom'
-import vault from '@factories/vault'
+import vault, {CredentialType} from '@factories/vault'
 import CredentialModal from '@src/components/modal/CredentialModal'
 import {useNotification} from '@src/components/NotificationToast'
+import {TrashCan} from '@src/components/svg/TrashCan'
+import {Copy} from '@src/components/svg/Copy'
 
 const Vault = () => {
-  const navigate = useNavigate()
+  const [credentials, setCredentials] = useState<(CredentialType & {id: string})[]>([])
   const {addNoty} = useNotification()
   const {vaultId} = useParams()
 
@@ -18,12 +20,28 @@ const Vault = () => {
   const init = async () => {
     try {
       const res = await vault.getOne(vaultId || '')
-
-
+      const json = JSON.parse(atob(res.data))
+      const ids = Object.keys(json)
+      const newCredentials: (CredentialType & {id: string})[] = []
+      for (const id of ids) {
+        newCredentials.push({
+          id,
+          name: json[id].name,
+          credential: json[id].credential,
+          url: json[id].url,
+          note: json[id].note,
+          password: json[id].password,
+        })
+      }
+      setCredentials(newCredentials)
     } catch (e: any) {
       addNoty(e.message, 'error')
     }
+  }
 
+  const clipboardCopy = (val: string) => {
+    navigator.clipboard.writeText(val)
+    addNoty('Password has been copied to clipboard', 'success', 'Copied!')
   }
 
   return (
@@ -37,31 +55,43 @@ const Vault = () => {
             <thead>
             <tr>
               <th>Name</th>
+              <th>Credential</th>
               <th>Password</th>
               <th>Created At</th>
               <th>Action</th>
             </tr>
             </thead>
             <tbody>
-            <tr>
-              <td>pass 1</td>
-              <td>********</td>
-              <td>{libDate.isoToDate1('2024-08-08T08:10:00Z')}</td>
-              <td>
-                <div className="flex gap-4">
-                  <button
-                    onClick={() => callModal()}>
-                    D
-                  </button>
-                  <p>V</p>
-                </div>
-              </td>
-            </tr>
+              {credentials.map((credential) => (
+                <tr>
+                  <td>{credential.name}</td>
+                  <td onClick={() => clipboardCopy(credential.credential)} className="cursor-pointer">
+                    <div className="flex justify-center gap-4">
+                      <span>********</span>
+                      <div className="w-4">
+                        <Copy/>
+                      </div>
+                    </div>
+                  </td>
+                  <td>{credential.password}</td>
+                  <td>{libDate.isoToDate1('2024-08-08T08:10:00Z')}</td>
+                  <td>
+                    <div className="flex gap-4">
+                      <button onClick={() => callModal()}>
+                        <div className="w-4">
+                          <TrashCan />
+                        </div>
+                      </button>
+                      <p>V</p>
+                    </div>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </section>
       </section>
-      <CredentialModal />
+      <CredentialModal/>
     </main>
   )
 }
